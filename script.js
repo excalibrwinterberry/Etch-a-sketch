8e024993671f6d6a9656b8f0ab528aa81bc70a10
arkaryan <arkaryangit@gmail.com>
2021-10-02 13:48:29 +0530

    added clear button and range slider

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-const n = 50
-const container = document.getElementsByClassName("container")[0]
+function setGrid(containerTag,n){
+    container.innerHTML = ""
+    container.style.gridTemplateColumns = `repeat(${n}, 2fr)`
+    container.style.gridTemplateRows = `repeat(${n}, 2fr)`
+    for(let i=1; i<=n*n; i++){
+        let cell = document.createElement('div')
+        cell.setAttribute('id', i)
+        cell.addEventListener('mouseenter', changeColor)
+        containerTag.appendChild(cell)
+    }
+    
+}
+
 
 function changeColor(event){
-    //    document.getElementById(`${event.target.id}`).classList.toggle('chosen')
-
-    document.getElementById(`${event.target.id}`).style.backgroundColor = "black"
+    document.getElementById(`${event.target.id}`).style.backgroundColor = drawColor
 }
 
-container.style.gridTemplateColumns = `repeat(${n}, 2fr)`
-container.style.gridTemplateRows = `repeat(${n}, 2fr)`
-
-for(let i=1; i<=n*n; i++){
-    let cell = document.createElement('div')
-    cell.setAttribute('id', i)
-    container.appendChild(cell)
+function revertColor(tag){
+    document.getElementById(`${tag.id}`).style.backgroundColor = defaultColor
 }
 
-for(let i=1; i<=n*n; i++){
-    let cell = document.getElementById(`${i}`)
-    cell.addEventListener('mouseenter', changeColor)
+let n =16
+
+const dimensionTag = document.getElementById("dimension")
+
+const dimensionDisplay = document.getElementById("rangeValue")
+dimensionDisplay.textContent = n
+
+const container = document.getElementsByClassName("container")[0]
+
+const drawColor = "black"
+const defaultColor = "LightGray"
+
+
+
+setGrid(container, n)
+
+dimensionTag.oninput = function() {
+    n = parseInt(this.value)
+    dimensionDisplay.textContent = n
+    setGrid(container, n)
 }
+
+
+
+//clear button
+document.querySelector("#clear").addEventListener("click", (e)=>{
+    const grid = [...container.children]
+
+    grid.forEach((cell)=>{
+        revertColor(cell)
+    })
+    
+})
+
